3d41e3b8af5cb64d1a97a24f32981cf99c6cde5c
thefallfram <kzkejiso@gmail.com>
2026-03-30 14:46:06 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ const listceh = ['poroshki/', 'medeplav/', 'electroliz/']
 const listnum = ['ques1.html', 'ques2.html', 'ques3.html', 'ques4.html', 'ques5.html', 'ques6.html', 'ques7.html', 'ques8.html', 'ques9.html', 'ques10.html']
 htmlname = window.location.pathname.split('/').pop()
 
+if (listnum.includes(htmlname)) {
+    const body = document.getElementById('body')
+    body.style.backgroundImage = "url('../pattern_vopros.png')"
+    
+}
+
 nav1.addEventListener('click', function () {
-    window.location.href = 'https://thefallfram.github.io/UGMKGames/index.html'
+    window.location.href = '../index.html'
 })
 nav2.addEventListener('click', function () {
-    window.location.href = 'https://thefallfram.github.io/UGMKGames/about.html'
+    window.location.href = '../about.html'
 })
 nav3.addEventListener('click', function () {
-    window.location.href = 'https://thefallfram.github.io/UGMKGames/play.html'
+    window.location.href = '../play.html'
 })
 nav4.addEventListener('click', function () {
-    window.location.href = 'https://thefallfram.github.io/UGMKGames/videogame.html'
+    window.location.href = '../videogame.html'
 })
 
 document.documentElement.style.setProperty('--width', width + 'px')
